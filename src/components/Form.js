eb1ecaea7597ff9b96ea303dee90412d30287419
mayurlalwani <mayur_lalwani4@ymail.com>
2020-05-21 21:55:16 +0530

    Added styles

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './form.css'
 // import {TextField, Button } from '@material-ui/core';
 
 function Form({returnGuess}){
@@ -11,13 +11,16 @@ function Form({returnGuess}){
     }
 
     return (
+        <div className="form-container">
+            <h2>Form</h2>
         <form style={{marginTop: '20px'}} onSubmit={handleChange}>
             {/* <TextField style={{paddingBottom: '20px'}} fullWidth type="number" name="guess" label="Enter your guess..." required /> */}
-            <input style={{paddingBottom: '20px'}} name="guess" label="Enter your guess..." required />
+            <input style={{paddingBottom: '20px'}} name="guess" placeholder="Guess a Number" required />
             <button>Submit</button>
             {/* <Button fullWidth variant="contained" color="primary">Submit</Button> */}
             
         </form>
+        </div>
     )
 }
 
