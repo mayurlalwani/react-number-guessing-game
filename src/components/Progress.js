a2dcef393218e96a1059784b85da24f7d8731adb
mayurlalwani <mayur_lalwani4@ymail.com>
2020-05-22 15:00:06 +0530

    Removed css import

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,14 +1,11 @@
 import React from 'react';
-// import {Typography} from '@material-ui/core';
-import './Progress.css'
-
 function Progress({attempt, guessList}){
     return (
         <div>
             {/* <Typography style={{marginTop:'50px'}}variant="h2">Guess {attempt} </Typography> */}
-            <h2>Guess {attempt}</h2>
+            <h2>Total Guess: {attempt}</h2>
             <ul className="progressBar_history">
-                {guessList}
+            All Guesses:-{guessList}
             </ul>
         </div>
     )
