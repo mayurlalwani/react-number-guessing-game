10e0983d26e24e7a587f856d4f5408ea0f89a839
mayurlalwani <mayur_lalwani4@ymail.com>
2020-05-22 16:26:48 +0530

    Improved naming convention

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,13 +1,15 @@
 import React from 'react';
 function Progress({attempt, guessList}){
     return (
-        <div>
+        <>
             {/* <Typography style={{marginTop:'50px'}}variant="h2">Guess {attempt} </Typography> */}
             <h2>Total Guess: {attempt}</h2>
-            <ul className="progressBar_history">
-            All Guesses:-{guessList}
-            </ul>
-        </div>
+            <div className="guesses">
+                <ul className="progressBar_history">
+                All Guesses:-{guessList}
+                </ul>
+            </div>
+        </>
     )
 }
 
