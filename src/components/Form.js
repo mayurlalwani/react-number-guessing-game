89860b17c09c1a71b1b3858730d2220c636e97a1
mayurlalwani <mayur_lalwani4@ymail.com>
2020-05-23 19:56:51 +0530

    Code refactor

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,26 @@
 import React from 'react';
 
-// import {TextField, Button } from '@material-ui/core';
+function Form({feedbackMsg,attempt,returnGuess}){
+    console.log(attempt);
 
-function Form({returnGuess}){
     function handleChange(e){
         e.preventDefault();
         const guess = e.target.elements.guess.value;
         returnGuess(guess);
-        e.target.elements.guess.value = ''
+        e.target.elements.guess.value = '';
     }
 
     return (
         <div className="form-container">
             
         <form style={{marginTop: '20px'}} onSubmit={handleChange}>
-            <input type="number" style={{paddingBottom: '20px'}} name="guess" placeholder="Guess a Number" required />
-            <button>GUESS</button>
+            {attempt === 10 ? 
+                    <input disabled placeholder="You Lost!! Reset the game to play again!" />  :
+                    
+                    <input type="number" name="guess" placeholder="Guess a Number" required />     
+            }
             
+            <button>GUESS</button>
         </form>
         </div>
     )
