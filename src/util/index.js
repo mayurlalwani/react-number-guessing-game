aa5948203ba0644276018c30a8f303b2afc3e94d
mayurlalwani <mayur_lalwani4@ymail.com>
2020-05-23 19:55:44 +0530

    Added state

--- src/util/index.js
+++ src/util/index.js
@@ -1,1 +1,10 @@
 export const generateRandomNumber = () => Math.floor(Math.random() *100 ) +1;
+
+export const initialState = () => ({
+    actualNumber:generateRandomNumber(),
+    guess:undefined,
+    allGuess: [],
+    attempt:0,
+    feedbackMessage:'Guess a number...',
+    seen:false
+});
