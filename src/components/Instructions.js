56d8c60a688e015fe7c8173481fc100da7595ef6
mayurlalwani <mayur_lalwani4@ymail.com>
2020-05-24 10:45:01 +0530

    Improved Instructions

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -11,8 +11,8 @@ export default class PopUp extends Component {
         <div className="modal_content">
           <h2>INSTRUCTIONS</h2>
           <ol>
-              <li>You need to guess a random number between 1 to 100 within 10 tries.</li>
-              
+              <li>Computer chooses a random number between 1 to 100 and keeps it hidden.</li>
+              <li>You have 10 tries to win this game.</li>
               <li>You will get feedback on how close or far your guess is in the form of the following keywords ("Small", "Too Small", "You are close", "You are closer!!!").</li>
           </ol>
           <span className="close" onClick={this.handleClick}>
