d091a8d8f3132048cfe3d29ee3908286665a7340
mayurlalwani <mayur_lalwani4@ymail.com>
2020-05-25 11:08:12 +0530

    Code refactor

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -11,8 +11,8 @@ export default class PopUp extends Component {
         <div className="modal_content">
           <h2>INSTRUCTIONS</h2>
           <ol>
-              <li>Computer chooses a random number between 1 to 100 and keeps it hidden.</li>
-              <li>You have 10 tries to win this game.</li>
+              <li>A random number between 1 to 100 is hidden.</li>
+              <li>You have to guess that random number within 10 tries to win this game.</li>
               <li>You will get feedback on how close or far your guess is in the form of the following keywords ("Small", "Too Small", "You are close", "You are closer!!!").</li>
           </ol>
           <span className="close" onClick={this.handleClick}>
